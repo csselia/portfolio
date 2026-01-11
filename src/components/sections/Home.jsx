c3406cd005ac4bdc10b4fcda35e6d1422e0ef87c
Cecilia <cecili26a@gmail.com>
2026-01-11 21:02:10 +0100

    update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -10,7 +10,7 @@ export const Home = ()=>{
         leading-tight">Hei! Jeg heter Cecilia</h1>
 
         <p className="text-grey-400 text-lg mb-8 mx-auto"> 
-            UX-fokusert full-stack utvikler. Kreativ designentusiast.  
+            UX-fokusert utvikler, 3.årsstudent Informasjonsteknologi
         </p>
 
             <div className="flex justify-center space-x-4">
